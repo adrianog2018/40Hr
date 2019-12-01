4bcc1720cd05ebb824624589a75ddf33f63ec5d9
Bryan Torreblanca <bryantorreblanca@Judys-MacBook-Air.local>
2019-12-01 17:59:05 -0500

    added time object to use for Employee

---
+++ Time.py
@@ -0,0 +1,25 @@
+import datetime as dt
+
+class Time:
+    
+    def __init__(self):
+        self.time_left = 40
+        self.overtime = 0
+
+    def log_hours(self, hrs):
+        assert(isinstance(hrs, int))
+        if (self.time_left - hrs >= 0):
+            self.time_left -= hrs
+        else:
+            self.overtime = hrs - self.time_left
+            self.time_left = 0
+    
+    def get_time_left(self):
+        return self.time_left
+        
+    def get_overtime(self):
+        return self.overtime
+    
+    def reset_hours(self):
+        self.time_left = 40
+    
